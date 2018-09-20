b0ae80486c5902d654db47553b289e676cc721d1
Zack Stout <zackstout@gmail.com>
2018-09-20 13:01:33 -0500

    figured out rotating the camera along with the player

---
+++ p1.cs
@@ -0,0 +1,30 @@
+
+using UnityEngine;
+
+public class cam : MonoBehaviour {
+
+	public Vector3 offset;
+	public bool isJumping = false;
+	public Rigidbody player;
+
+
+	// Going to be a bit more involved to ensure that MOVEMENT keys move us RELATIVE to our current angle...
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("ahoy hoy!" + player.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		offset.z = -5 * Mathf.Cos(player.transform.rotation.eulerAngles.y * Mathf.Deg2Rad);
+
+		offset.x = -5 * Mathf.Sin (player.transform.rotation.eulerAngles.y * Mathf.Deg2Rad);
+
+		transform.position = player.transform.position + offset;
+
+		transform.rotation = Quaternion.Euler (0, player.transform.rotation.eulerAngles.y, 0);
+
+	}
+}
